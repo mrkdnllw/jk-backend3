db8576f2ef1304a49fc72db54df1c78f5fc38d72
mrkdnllw <mrkdnlph@gmail.com>
2023-04-22 07:47:11 +0800

    my versions

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 import express from "express";
 import { connectToDatabase } from "./database";
-
+import { connectToDatabase2 } from "./database2";
 import { employeeRouter } from "./employee.routes";
+
+import { userRouter } from "./user.routes";
 
 // Load environment variables from the .env file, where the ATLAS_URI is configured
 dotenv.config();
@@ -15,29 +17,61 @@ if (!ATLAS_URI) {
     process.exit(1);
 }
 
-connectToDatabase(ATLAS_URI)
-    .then(() => {
-        const app = express();
+// connectToDatabase(ATLAS_URI)
+//     .then(() => {
+//         const app = express();
 
-        // const allowedOrigins = ['http://hello.com', 'http://google.com/', 'http://localhost:5200/', 'https://localhost:5200/', 'localhost:5200/'];
+//         // const allowedOrigins = ['http://hello.com', 'http://google.com/', 'http://localhost:5200/', 'https://localhost:5200/', 'localhost:5200/'];
 
-        // app.use(cors({
-        //     origin: function (origin, callback) {
-        //         if (allowedOrigins.indexOf(origin) !== -1) {
-        //             callback(null, true);
-        //         } else {
-        //             callback(new Error('Not allowed by CORS'));
-        //         }
-        //     }
-        // }));
+//         // app.use(cors({
+//         //     origin: function (origin, callback) {
+//         //         if (allowedOrigins.indexOf(origin) !== -1) {
+//         //             callback(null, true);
+//         //         } else {
+//         //             callback(new Error('Not allowed by CORS'));
+//         //         }
+//         //     }
+//         // }));
 
-        //before ni sha then bunch of endpoints
-        app.use("/employees", employeeRouter);
+//         //before ni sha then bunch of endpoints
+//         app.use("/employees", employeeRouter);
 
-        // start the Express server
-        app.listen(5200, () => {
-            console.log(`Server running at http://localhost:5200...`);
-        });
+//         // start the Express server
+//         app.listen(5200, () => {
+//             console.log(`Server running at http://localhost:5200...`);
+//         });
 
-    })
-    .catch(error => console.error(error));
+//     })
+//     .catch(error => console.error(error));
+
+
+
+    
+connectToDatabase2(ATLAS_URI)
+.then(() => {
+    const app = express();
+
+    // const allowedOrigins = ['http://hello.com', 'http://google.com/', 'http://localhost:5200/', 'https://localhost:5200/', 'localhost:5200/'];
+
+    // app.use(cors({
+    //     origin: function (origin, callback) {
+    //         if (allowedOrigins.indexOf(origin) !== -1) {
+    //             callback(null, true);
+    //         } else {
+    //             callback(new Error('Not allowed by CORS'));
+    //         }
+    //     }
+    // }));
+
+    app.use(cors({}));
+
+    //before ni sha then bunch of endpoints
+    app.use("/users", userRouter);
+
+    // start the Express server
+    app.listen(5200, () => {
+        console.log(`Server running at http://localhost:5200...`);
+    });
+
+})
+.catch(error => console.error(error));
---
+++ src/user.ts
@@ -0,0 +1,9 @@
+import * as mongodb from "mongodb";
+ 
+export interface User {
+    username?: string;
+    password?: string;
+    email?: string;
+   _id?: mongodb.ObjectId;
+}
+
