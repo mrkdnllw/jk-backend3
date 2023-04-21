bfe60e38a3e9bbef6deb80c400e7c05ec6705bd8
mrkdnllw <mrkdnlph@gmail.com>
2023-04-21 17:34:06 +0800

    rearranging stuff

--- src/server.ts
+++ src/server.ts
@@ -4,28 +4,40 @@ import express from "express";
 import { connectToDatabase } from "./database";
 
 import { employeeRouter } from "./employee.routes";
- 
+
 // Load environment variables from the .env file, where the ATLAS_URI is configured
 dotenv.config();
- 
-const { ATLAS_URI } = process.env;
- 
-if (!ATLAS_URI) {
-   console.error("No ATLAS_URI environment variable has been defined in config.env");
-   process.exit(1);
-}
- 
-connectToDatabase(ATLAS_URI)
-   .then(() => {
-       const app = express();
-       app.use(cors());
 
-       app.use("/employees", employeeRouter);
- 
-       // start the Express server
-       app.listen(5200, () => {
-           console.log(`Server running at http://localhost:5200...`);
-       });
- 
-   })
-   .catch(error => console.error(error));
+const { ATLAS_URI } = process.env;
+
+if (!ATLAS_URI) {
+    console.error("No ATLAS_URI environment variable has been defined in config.env");
+    process.exit(1);
+}
+
+connectToDatabase(ATLAS_URI)
+    .then(() => {
+        const app = express();
+
+        // const allowedOrigins = ['http://hello.com', 'http://google.com/', 'http://localhost:5200/', 'https://localhost:5200/', 'localhost:5200/'];
+
+        // app.use(cors({
+        //     origin: function (origin, callback) {
+        //         if (allowedOrigins.indexOf(origin) !== -1) {
+        //             callback(null, true);
+        //         } else {
+        //             callback(new Error('Not allowed by CORS'));
+        //         }
+        //     }
+        // }));
+
+        //before ni sha then bunch of endpoints
+        app.use("/employees", employeeRouter);
+
+        // start the Express server
+        app.listen(5200, () => {
+            console.log(`Server running at http://localhost:5200...`);
+        });
+
+    })
+    .catch(error => console.error(error));
